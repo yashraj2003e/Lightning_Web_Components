d9d398a63fc63bb30c680366e075a18e93855f91
Yashraj <yashsharmau9@gmail.com>
2025-10-27 21:19:23 +0530

    Added Lightning View Record Form

--- force-app/main/default/lwc/displayAccount/displayAccount.js
+++ force-app/main/default/lwc/displayAccount/displayAccount.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class DisplayAccount extends LightningElement {}
+import accName from "@salesforce/schema/Account.Name";
+import Industry from "@salesforce/schema/Account.Industry";
+import Phone from "@salesforce/schema/Account.Phone";
+
+export default class DisplayAccount extends LightningElement {
+  @api recordId;
+  @api objectApiName;
+  fields = [accName, Industry, Phone];
+}
